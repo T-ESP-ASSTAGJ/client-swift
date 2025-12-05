a0fa505f1f17964e5184073ef0df46c21f15c3bb
reverss <reverssecole@gmail.com>
2025-12-05 23:23:22 +0100

    Initial Commit

---
+++ jamlyTests/jamlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  jamlyTests.swift
+//  jamlyTests
+//
+//  Created by REVERSS on 05/12/2025.
+//
+
+import Testing
+@testable import jamly
+
+struct jamlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
