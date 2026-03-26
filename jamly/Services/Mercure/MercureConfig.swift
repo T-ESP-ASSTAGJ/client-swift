32e7eae3f4dc676b3dd162ddba1ca2f80f1ea3ca
jonathandumesnil <jonathan.dumesnil@epitech.eu>
2026-03-26 16:02:29 +0100

    :sparkles: TEM-273 déletion des ip

--- jamly/Services/Mercure/MercureConfig.swift
+++ jamly/Services/Mercure/MercureConfig.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Configuration simple pour Mercure
 struct MercureConfig {
-    static let hubURL = "http://10.68.252.15:80/.well-known/mercure"
+    static let hubURL = "http://ip-locale:80/.well-known/mercure"
     
     static var token: String {
         SecureStore.shared.retrieve() ?? ""
