d5fc4ccb97c8e15b205b59d9610a4b3500055aa6
jonathandumesnil <jonathan.dumesnil@epitech.eu>
2026-04-09 14:09:22 +0200

    :construction: Rebase sur staging

--- jamly/Services/Mercure/MercureConfig.swift
+++ jamly/Services/Mercure/MercureConfig.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Configuration simple pour Mercure
 struct MercureConfig {
-    static let hubURL = "http://ip-locale:80/.well-known/mercure"
+    static let hubURL = "\(Config.baseURL)/.well-known/mercure"
     
     static var token: String {
         SecureStore.shared.retrieve() ?? ""
