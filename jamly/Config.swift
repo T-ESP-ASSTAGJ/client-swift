2305c92f46a81615dab64535c40ef5df1e7a84fc
reverss <reverssecole@gmail.com>
2026-04-09 11:21:01 +0200

    :wrench: TEM-291 Centralisation de la baseURL via Config.swift #STAGING

---
+++ jamly/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  Jamly
+//
+//  Created by REVERSS on 20/03/2026.
+//
+
+enum Config {
+#if DEBUG
+    static let baseURL = "http://10.68.251.255:80"
+#else
+    static let baseURL = "https://api.jamly.app"
+#endif
+}
+
