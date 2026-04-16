ffb4c71c0c9c79a9fd0c3dff4c38607ee98e5318
jonathandumesnil <jonathan.dumesnil@epitech.eu>
2026-04-16 12:07:17 +0200

    :construction: remove notification view

--- jamly/App/Profile/ProfileView.swift
+++ jamly/App/Profile/ProfileView.swift
@@ -30,7 +30,6 @@ struct ProfileView: View {
     @State private var selectedPost: Post? = nil
     @State private var isShowingPostDetail = false
     @State private var isFollowingTarget: Bool = false
-@State private var showNotifications = false
     @State private var unreadNotificationsCount = 2 // TODO: Remplacer par vraie valeur depuis API
 
     // MARK: - Computed Properties
@@ -157,9 +156,6 @@ struct ProfileView: View {
         }
         .navigationDestination(isPresented: $showProfileEdit) {
             ProfileSettingsMenuView()
-        }
-        .navigationDestination(isPresented: $showNotifications) {
-            NotificationsView()
         }
         .navigationDestination(item: $selectedFollowView) { view in
             switch view {
@@ -379,7 +375,6 @@ struct ProfileView: View {
         selectedPost = nil
         showMusicPlaylists = false
         showProfileEdit = false
-        showNotifications = false
     }
 }
 
